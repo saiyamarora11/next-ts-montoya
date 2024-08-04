a9260e86140869663351cd8e852fe863b407af1a
saiyamarora11 <75494742+saiyamarora11@users.noreply.github.com>
2024-08-04 23:05:51 +0530

    fixes

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,19 +1,44 @@
 "use client";
 
+import { useInView, useScroll } from "framer-motion";
 import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
 
 const Card = ({ imgUrl }: { imgUrl: string }) => {
-  return (
-    <div className="relative h-[80vh] w-[90vw] bg-neutral-200 rounded-xl overflow-hidden">
-      <Image
-        src={imgUrl}
-        alt={imgUrl}
-        fill
-        className="object-cover"
-        sizes="90vw"
-      />
-    </div>
-  );
+	const vertMargin = 10;
+	const container = useRef(null);
+	const { scrollY } = useScroll({
+		target: container,
+	});
+	const [maxScrollY, setMaxScrollY] = useState(Infinity);
+
+	const isInView = useInView(container, {
+		margin: `0px 0px -${100 - vertMargin}% 0px`,
+		once: true,
+	});
+	useEffect(() => {
+		if (isInView) {
+			setMaxScrollY(scrollY.get());
+		}
+	}, [isInView]);
+
+	return (
+		<div
+			ref={container}
+			className="sticky h-[80vh] w-[90vw] overflow-hidden rounded-xl bg-neutral-200"
+			style={{
+				top: `${vertMargin}vh`,
+				height: `${100 - 2 * vertMargin}vh`,
+			}}>
+			<Image
+				src={imgUrl}
+				alt={imgUrl}
+				fill
+				className="object-cover"
+				sizes="90vw"
+			/>
+		</div>
+	);
 };
 
 export default Card;
