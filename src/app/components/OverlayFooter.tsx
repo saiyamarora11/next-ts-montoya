5e153ecbe4578b372ac895c46abfa22948f1f120
saiyamarora11 <75494742+saiyamarora11@users.noreply.github.com>
2024-08-06 03:31:56 +0530

    fixes

--- src/app/components/OverlayFooter.tsx
+++ src/app/components/OverlayFooter.tsx
@@ -8,7 +8,7 @@ const OverlayFooter = ({ title }: OverlayFooterProps) => {
 	return (
 		<div className="overlay-footer">
 			<button className="tag-overlay">2024</button>
-			<h1 className="font-sixCaps text-8xl font-bold">{title}</h1>
+			<h1 className="overlay-title-style">{title}</h1>
 			<span className="tag-overlay">PHOTO</span>
 		</div>
 	);
