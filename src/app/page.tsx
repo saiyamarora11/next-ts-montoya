343cb8930b4a29bc9371a057590ee7ec84eee388
saiyamarora11 <75494742+saiyamarora11@users.noreply.github.com>
2024-08-15 11:47:03 +0530

    padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import {
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center">
+		<main className="flex min-h-screen flex-col items-center px-6 sm:px-14">
 			<MagicCursor />
 			<Header />
 			<HeroSection />
