71bd4c29aac0504f7e07befadd1d6515236363de
saiyamarora11 <75494742+saiyamarora11@users.noreply.github.com>
2024-08-17 15:52:50 +0530

    fixes

--- src/app/hooks/useLenis.ts
+++ src/app/hooks/useLenis.ts
@@ -8,19 +8,12 @@ const useLenis = () => {
 
 	const initializeLenis = useCallback(
 		(lerp: number) => {
-			const isMobile = () => window.innerWidth <= 768;
-			const wheelMultiplier = isMobile() ? 0.2 : 0.5;
-			const touchMultiplier = isMobile() ? 0.2 : 0.5;
-
 			lenis.current = new Lenis({
 				lerp,
 				smoothWheel: true,
 				syncTouch: true,
 				syncTouchLerp: 0.1,
-				wheelMultiplier,
-				touchMultiplier,
 			});
-
 			const raf = (time: number) => {
 				lenis.current?.raf(time);
 				requestAnimationFrame(raf);
@@ -36,22 +29,15 @@ const useLenis = () => {
 
 		return () => lenis.current?.destroy();
 	}, [initializeLenis]);
-
 	useEffect(() => {
 		if (lenis.current) {
 			console.log(
 				`Setting scroll speed to ${inSkillsSection ? "slow" : "normal"}`,
 			);
 			lenis.current.destroy();
-			initializeLenis(
-				inSkillsSection
-					? window.innerWidth <= 768
-						? 0.02
-						: 0.005
-					: 0.1,
-			);
+			initializeLenis(inSkillsSection ? 0.005 : 0.1);
 		}
-	}, [inSkillsSection, initializeLenis]);
+	}, [inSkillsSection]);
 
 	return { adjustScrollSpeed: lenis.current?.options.lerp };
 };
